ebcd4079c3a2187b734763d8a67c87f5f5a3374c
Hritik2816 <133709349+Hritik2816@users.noreply.github.com>
2025-02-19 14:43:26 +0530

    RESTApi

---
+++ REST_CLASS/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"))
+
+app.use(express.static(path.join(__dirname, "public")))
+
+
+let posts = [
+  {
+    username: "hritik2816",
+    content: "This is the file of Hritik Gupta "
+  },
+  {
+    username: "bhawna28",
+    content: "This is the file of Bhawna Gupta "
+  },
+  {
+    username: "hrk16",
+    content: "This is the file of Hritik Kumar "
+  },
+]
+
+app.get("/posts", (req, res) => {
+  res.render("index.ejs", { posts });
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+})
