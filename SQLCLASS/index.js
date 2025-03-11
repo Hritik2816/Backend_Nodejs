259a82ebaa604fe4d95f51f368c7cfb0201b94d1
Hritik2816 <133709349+Hritik2816@users.noreply.github.com>
2025-03-11 13:41:29 +0530

    ejs where create home page and number of users

--- SQLCLASS/index.js
+++ SQLCLASS/index.js
@@ -1,7 +1,11 @@
 const { faker } = require('@faker-js/faker');
 const mysql = require('mysql');
-const express = required('express');
+const express = require('express');
 const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
 
 // Create the connection to database
 const connection = mysql.createConnection({
@@ -27,11 +31,22 @@ let createRandomUser = () => {
 // }
 
 app.get("/", (req, res) => {
-  res.send('Hello World');
+  let q = "SELECT COUNT(*) FROM user";
+  try {
+    connection.query(q, (error, result) => {
+      if (error) throw error;
+      let count = result[0]["COUNT(*)"];
+      res.render('home.ejs', { count });
+    })
+  } catch (error) {
+    console.log('Error connecting to database:')
+    res.send('SOMETHING WENT WRONG');
+
+  }
 });
 
-app.listen(3000, () => {
-  log('Server is running on port 3000');
+app.listen(8080, () => {
+  console.log('Server is running on port 8080');
 })
 
 // try {
